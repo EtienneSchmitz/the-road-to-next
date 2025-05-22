120b789b0933911b108cd02d6e34cfb747f4b1f6
Etienne SCHMITZ <contact@etienne-schmitz.com>
2025-05-22 22:53:41 +0200

    fix : errors with the title

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -8,7 +8,7 @@ const TicketsPage = () => {
     return (
         <div className="flex-1 flex flex-col gap-y-8">
         
-        <Heading title="TicketsPage" description="All your tickets at one place" />
+        <Heading title="Tickets" description="All your tickets at one place" />
 
             <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
                 {initialTickets.map((ticket) => (
