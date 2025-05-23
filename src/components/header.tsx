4ad46fe3c0ea735db88cf819a023b0963929c25f
Etienne SCHMITZ <contact@etienne-schmitz.com>
2025-05-24 01:34:45 +0200

    feat : add dark theme

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,7 @@
 import { LucideKanban } from "lucide-react";
 import Link from "next/link";
 import { homePath, ticketsPath } from "@/paths";
+import { ThemeSwitcher } from "./theme/theme-switcher";
 import { buttonVariants } from "./ui/button";
 
 const Header = () => {
@@ -11,13 +12,14 @@ const Header = () => {
             border-b bg-background/95 backdrop-blur
             w-full flex py-2.5 px-5 justify-between
           ">
-          <div>
+          <div className="flex align-items gap-x-2">
             <Link href={homePath()} className={buttonVariants({ variant: 'ghost' })}>
               <LucideKanban />
               <h1 className="text-lg font-semibold">TicketBounty</h1>
             </Link>
           </div>
-          <div>
+          <div className="flex align-items gap-x-2">
+            <ThemeSwitcher />
             <Link href={ticketsPath()} className={buttonVariants({ variant: 'default' })}>Tickets</Link>
           </div>
         </nav>
