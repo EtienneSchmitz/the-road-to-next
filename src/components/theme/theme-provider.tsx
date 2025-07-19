3a5d348848e37fe6193488427ed26fba4775ba18
Etienne SCHMITZ <contact@etienne-schmitz.com>
2025-07-20 01:59:44 +0200

    feat - finish section 7 and 8 : Feature library and dark component (2)

--- src/components/theme/theme-provider.tsx
+++ src/components/theme/theme-provider.tsx
@@ -1,10 +1,24 @@
+"use client";
+
 import { ThemeProvider as BaseThemeProvider } from "next-themes";
+import { useEffect, useState } from "react";
 
 type ThemeProviderProps = {
   children: React.ReactNode;
 };
 
 const ThemeProvider = ({ children }: ThemeProviderProps) => {
+  const [mounted, setMounted] = useState(false);
+
+  // useEffect only runs on the client, so now we can safely show the UI
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
   return (
     <BaseThemeProvider attribute="class" defaultTheme="system" enableSystem>
       {children}
